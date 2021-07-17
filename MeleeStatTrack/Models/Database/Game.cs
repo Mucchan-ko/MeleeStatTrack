4240d25fd20fc7f5f0c80cb4296b952c36e1a9be
Nick Katarzynski <otomiru@hotmail.com>
2021-07-16 21:48:34 -0400

    Initial class structure

---
+++ MeleeStatTrack/Models/Database/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeleeStatTrack.Models.Database
+{
+    public class Game
+    {
+    }
+}
